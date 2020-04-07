36e3885d2cf27150ab7bd772ec11011bbfd69a7c
S'unya <sunya.dickman@aerian.com>
2020-04-07 09:36:01 +0100

    feat: changed a11y rules

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -2,14 +2,15 @@ module.exports = {
     extends: [
         "plugin:import/react",
         "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended",
         "@aerian/eslint-config",
-        "prettier/react"
+        "prettier/react",
     ],
     plugins: ["react-hooks", "jsx-a11y"],
     parserOptions: {
         ecmaFeatures: {
-            jsx: true
-        }
+            jsx: true,
+        },
     },
     rules: {
         "react/jsx-indent": "off",
@@ -18,7 +19,7 @@ module.exports = {
         "react/jsx-curly-spacing": "off",
         "react/jsx-filename-extension": [
             1,
-            { extensions: [".js", ".jsx", ".ts", ".tsx"] }
+            { extensions: [".js", ".jsx", ".ts", ".tsx"] },
         ],
         "react/prop-types": "off",
         "react-hooks/rules-of-hooks": "error",
@@ -26,34 +27,11 @@ module.exports = {
         "react/style-prop-object": "error",
         "react/no-typos": "error",
         "react/no-unescaped-entities": "off",
-        "jsx-a11y/accessible-emoji": "warn",
-        "jsx-a11y/alt-text": "warn",
-        "jsx-a11y/anchor-has-content": "warn",
-        "jsx-a11y/anchor-is-valid": [
-            "warn",
-            {
-                aspects: ["noHref", "invalidHref"]
-            }
-        ],
-        "jsx-a11y/aria-activedescendant-has-tabindex": "warn",
-        "jsx-a11y/aria-props": "warn",
-        "jsx-a11y/aria-proptypes": "warn",
-        "jsx-a11y/aria-role": "warn",
-        "jsx-a11y/aria-unsupported-elements": "warn",
-        "jsx-a11y/heading-has-content": "warn",
-        "jsx-a11y/iframe-has-title": "warn",
-        "jsx-a11y/img-redundant-alt": "warn",
-        "jsx-a11y/no-access-key": "warn",
-        "jsx-a11y/no-distracting-elements": "warn",
-        "jsx-a11y/no-redundant-roles": "warn",
-        "jsx-a11y/role-has-required-aria-props": "warn",
-        "jsx-a11y/role-supports-aria-props": "warn",
-        "jsx-a11y/scope": "warn"
     },
     settings: {
         "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
         react: {
-            version: "detect"
-        }
-    }
+            version: "detect",
+        },
+    },
 };
